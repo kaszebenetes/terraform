26b1d45ebbb6d6f21003a2dfb73330ce98b90c35
Kobeep <kuba.pospieszny@gmail.com>
2024-07-24 10:30:29 +0200

    Create Load-balancer

--- src/main/vm.tf
+++ src/main/vm.tf
@@ -1,27 +1,6 @@
-resource "azurerm_public_ip" "vm-pip" {
-  name                = "tf-vm-nic-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-
-  tags = var.tags
-}
-
-resource "azurerm_network_interface" "vm-nic" {
-  name                = "vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet[1].id
-    public_ip_address_id          = azurerm_public_ip.vm-pip.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "vm-linux" {
-  name                = "tf-vm-linux"
+  count = 2
+  name                = "tf-vm-linux-${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B2ats_v2"
@@ -45,6 +24,28 @@ resource "azurerm_linux_virtual_machine" "vm-linux" {
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
+  }
+}
+
+resource "azurerm_public_ip" "vm-pip" {
+  name                = "tf-vm-nic-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface" "vm-nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet[1].id
+    public_ip_address_id          = azurerm_public_ip.vm-pip.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
