0b14861aacdbdd05d0e49254b60a0edfbdb8534e
Kobeep <kuba.pospieszny@gmail.com>
2024-07-30 13:33:28 +0200

    Ansible fix

--- src/ansible/vms/lb_config/firewall.sh
+++ src/ansible/vms/lb_config/firewall.sh
@@ -2,26 +2,16 @@
 
 set -x
 # set -euo pipefail
-
-if yum list installed | grep -q iptables
-then
-    echo "==> Info: Iptables service is already installed."
-else
-    
-    echo "==> Info: Installing Iptables service:"
-    echo "==> Info: Before installing Iptables , Firewalld is gonna to be disabled..."
-        systemctl stop firewalld
-        systemctl disable firewalld
-        systemctl mask firewalld
-    yum -y install iptables-services
-    echo "==> Info: Starting Iptables services..."
-        systemctl start iptables.service
-        systemctl enable iptables.service
-fi
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl mask firewalld
+sudo apt-get install iptable
+systemctl start iptables.service
+systemctl enable iptables.service
 
 iptables -I OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -s 10.0.0.111 --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp -s 10.0.2.4 --dport 22 -j ACCEPT
 iptables -I OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -I OUTPUT -p tcp --dport 443 -j ACCEPT
 iptables -I INPUT -p tcp --dport 80 -j ACCEPT
