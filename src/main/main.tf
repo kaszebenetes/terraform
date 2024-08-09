f75682f459d8d14cb598d75f6e737a382d1ab0eb
Kobeep <kuba.pospieszny@gmail.com>
2024-08-09 13:13:25 +0200

    clean

--- src/main/main.tf
+++ src/main/main.tf
@@ -36,13 +36,7 @@ module "subnet" {
   resource_group_name = azurerm_resource_group.rg.name
   vnet_name           = module.vnet.vnet_name
   address_prefixes    = each.value["address_prefixes"]
-<<<<<<< HEAD
-  project_prefix      = var.project_prefix
   tags                = var.tags
-
-=======
-  tags                = var.tags
->>>>>>> b96973fdeff16316e1e19a0e88070c4708df1460
 }
 
 module "web_vm" {
@@ -74,21 +68,12 @@ module "web_vm" {
 #   location            = azurerm_resource_group.rg.location
 #   vm_size             = "Standard_B2ats_v2"
 
-<<<<<<< HEAD
 #   # NIC config  --->
 #   private_ip_address = "10.0.1.4"
 #   subnet_id          = module.subnet.lb_subnet.id
 #   nsg_id             = azurerm_network_security_group.nsg-lb.id
 #   # PIP config  --->
 #   pip_enabled = true
-=======
-  # NIC config  --->
-  private_ip_address = "10.0.1.4"
-  subnet_id          = module.subnet.lb_subnet.id
-  nsg_id             = azurerm_network_security_group.nsg-lb.id
-  # PIP config  --->
-  pip_enabled = true
->>>>>>> b96973fdeff16316e1e19a0e88070c4708df1460
 
 #   # Bootdiagnostic--->
 #   boot_diagnostics_st_uri = azurerm_storage_account.diagstorage.primary_blob_endpoint
