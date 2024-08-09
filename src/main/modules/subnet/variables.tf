d73556c7658f1e5fe6c7e29b102b570f1c752f43
Kobeep <kuba.pospieszny@gmail.com>
2024-08-09 13:17:25 +0200

    vars pull

--- src/main/modules/subnet/variables.tf
+++ src/main/modules/subnet/variables.tf
@@ -1,9 +1,7 @@
 variable "tags" {
   type = map(string)
 }
-variable "project_prefix" {
-  type = string
-}
+
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
