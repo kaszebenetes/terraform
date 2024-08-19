bfda6a133eb4707f5a7b2e31a17c1dc94eb33d3f
Kobeep <kuba.pospieszny@gmail.com>
2024-08-19 14:48:47 +0200

    Add azure bastion

---
+++ src/main/bastion.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "bastion-pip" {
+  name                = "bastion-pip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "bastion"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.basion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion-pip.id
+  }
+}
